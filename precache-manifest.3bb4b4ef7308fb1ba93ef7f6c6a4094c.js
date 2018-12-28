8212555bba3bff71ddca0ff8be7732051d48ba36
Timothy Hobbs <timothyhobbs@seznam.cz>
2018-12-28 15:11:38 +0100

    Initial commit

---
+++ precache-manifest.3bb4b4ef7308fb1ba93ef7f6c6a4094c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dde83682deaa4576e0b",
+    "url": "/static/css/main.608ac7aa.chunk.css"
+  },
+  {
+    "revision": "3dde83682deaa4576e0b",
+    "url": "/static/js/main.3dde8368.chunk.js"
+  },
+  {
+    "revision": "8fdbd49a80d46923b92f",
+    "url": "/static/js/1.8fdbd49a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a339183e8b4ebaa932bbbeae32625e7e",
+    "url": "/index.html"
+  }
+];
